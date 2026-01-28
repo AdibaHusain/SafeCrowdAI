9a1e20b7f7e1a3d41d1cbe6a53a61bc8d7363254
Adiba <adibaknp@gmail.com>
2026-01-28 13:47:26 +0530

    Changes the version of YOLO

--- backend/app/services/crowd_detector.py
+++ backend/app/services/crowd_detector.py
@@ -2,14 +2,32 @@ from ultralytics import YOLO
 import cv2
 
 class CrowdDetector:
-    def __init__(self, model_path: str = "yolov8n.pt"):
+    def __init__(self, model_path: str = "yolov8s.pt"):
         self.model = YOLO(model_path)
 
     def count_people_in_image(self, image_path: str):
-        results = self.model(image_path)
+        results = self.model(image_path,conf=0.3)
         people_count = 0
         for result in results:
             for box in result.boxes:
-                if int(box.cls[0]) == 0:  # Class 0 = person in YOLO
+                if int(box.cls[0]) == 0:
+
+                    confidence = float(box.conf[0])
+
+                    # 🔹 Step 2: confidence check
+                    if confidence < 0.3:
+                        continue
+
+                    # 🔹 Step 3: box size filter
+                    x1, y1, x2, y2 = box.xyxy[0]
+                    width = x2 - x1
+                    height = y2 - y1
+                    area = width * height
+
+                    if area < 500:
+                        continue
+
+                    # 🔹 Valid person
                     people_count += 1
+
         return people_count
